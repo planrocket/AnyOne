0e5b11671c2e7129cbdd50961310631881a3b89c
tany <tany@qq.com>
2019-08-23 18:46:53 +0800

    add gradle opt

---
+++ group_apps/app/src/main/java/com/xin/anyone/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.xin.anyone.splash;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.xin.anyone.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
