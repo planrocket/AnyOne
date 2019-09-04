307d477bab06d1496d4e6c66d7370e183ddb0dca
tany <tany@qq.com>
2019-09-04 18:59:13 +0800

    change

---
+++ demo/src/main/java/com/example/demo/one/OneActivity.java
@@ -0,0 +1,15 @@
+package com.example.demo.one;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.demo.R;
+
+public class OneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+}
