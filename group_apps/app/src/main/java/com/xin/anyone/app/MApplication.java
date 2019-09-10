335f4c1b726bd614a871ea914bdaa1cdb96e6cc9
tany <tany@qq.com>
2019-09-10 16:15:33 +0800

    规范init任务。各个模块内的功能初始化 封装在 模块内部，对外提供public static void init 方法

--- group_apps/app/src/main/java/com/xin/anyone/app/MApplication.java
+++ group_apps/app/src/main/java/com/xin/anyone/app/MApplication.java
@@ -3,7 +3,7 @@ package com.xin.anyone.app;
 import android.app.Application;
 
 import com.xin.anyone.BuildConfig;
-import com.xin.config.GlobalConfig;
+import com.xin.common.init.CommonInitTask;
 
 public class MApplication extends Application {
 
@@ -11,17 +11,9 @@ public class MApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        injectGlobalConfig();
+        //初始化
+        CommonInitTask.initByMainProcess(this, BuildConfig.DEBUG);
     }
 
 
-    /**
-     * 注入全局 配置信息
-     * 仅支持部分全局参数（不含业务类逻辑）
-     */
-    private void injectGlobalConfig() {
-        GlobalConfig.Configer configer = new GlobalConfig.Configer();
-        configer.withIsDebug(BuildConfig.DEBUG);
-        configer.justConfigOnce();
-    }
 }
---
+++ group_common/base/src/main/java/com/xin/common/init/CommonInitTask.java
@@ -0,0 +1,30 @@
+package com.xin.common.init;
+
+import android.content.Context;
+
+import com.blankj.utilcode.util.Utils;
+import com.xin.config.GlobalConfig;
+
+public class CommonInitTask {
+
+    /**
+     * @param appContext Application的context
+     * @param isDebug    app是否debug模式
+     */
+    public static void initByMainProcess(Context appContext, boolean isDebug) {
+        initUtilCode(appContext);
+        initGlobalConfig(isDebug);
+    }
+
+
+    private static void initUtilCode(Context appContext) {
+        Utils.init(appContext);
+    }
+
+
+    private static void initGlobalConfig(boolean isDebug) {
+        GlobalConfig.Configer configer = new GlobalConfig.Configer();
+        configer.withIsDebug(isDebug);
+        configer.justConfigOnce();
+    }
+}
